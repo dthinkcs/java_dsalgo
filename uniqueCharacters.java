de324c9892acf38f18c9b9b457c4e0d874df31cb
rishabh <31166087+dthinkcs@users.noreply.github.com>
2018-03-30 20:59:36 +0530

    Create uniqueCharacters.java

---
+++ uniqueCharacters.java
@@ -0,0 +1,10 @@
+class Unique {
+  public static boolean isUnique(String s) {
+    boolean ascii_set = new boolean[256];
+    for (int i = 0, len = s.length(); i < len; i++) {
+      if (ascii_set[s.charAt(i)])
+        return false;
+    }
+    return true;
+  }
+}
